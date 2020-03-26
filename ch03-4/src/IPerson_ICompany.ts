76b138bb15160d76a0e11dda15e77b8fbb612610
gibaek Park <yaqqww@nate.com>
2020-03-26 17:16:08 +0900

    add: ch03-4 구조화, 비구조화 할당

---
+++ ch03-4/src/IPerson_ICompany.ts
@@ -0,0 +1,19 @@
+// let personName = 'Jack';
+// let personAge = 32;
+
+// let companyName = 'Apple Company, Inc';
+// let companyAge = 43;
+
+// 위에는 확장이 어려움
+
+export interface IPerson {
+  name: string
+  age: number
+}
+
+export interface ICompany {
+  name: string
+  age: number
+}
+
+// 구조화
