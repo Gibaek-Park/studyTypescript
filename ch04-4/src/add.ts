691af5282612bfd6ce8ea9d94a08327209a8ce7b
gibaek Park <yaqqww@nate.com>
2020-03-27 20:14:34 +0900

    add: 중첩 함수 예시

---
+++ ch04-4/src/add.ts
@@ -0,0 +1,9 @@
+type NumberToNumberFunc = (number: number) => number
+const add = (a: number): NumberToNumberFunc => {
+  const _add: NumberToNumberFunc = (b: number): number => {
+    return a + b;
+  }
+  return _add;
+}
+
+console.log(add(1)(2))
