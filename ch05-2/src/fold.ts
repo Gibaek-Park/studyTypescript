654614688f41c65c75b0d3743609c8a743073c9f
gibaek Park <yaqqww@nate.com>
2020-03-29 16:37:40 +0900

    add: 선언형 프로그램

---
+++ ch05-2/src/fold.ts
@@ -0,0 +1,8 @@
+export const fold = <T>(array: T[], callback: (result: T, value: T) => T, initValue: T): T => {
+  let result: T = initValue;
+  for(let i=0; i<array.length; ++i) {
+    const value = array[i];
+    result = callback(result, value)
+  }
+  return result;
+}
